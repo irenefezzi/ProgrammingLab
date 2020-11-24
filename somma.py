5d920d160a60493e0f2fc3c592b5a80cd214f191
repl.it user <replituser@example.com>
2020-11-24 15:13:13 +0000

    aggiungo file

---
+++ somma.py
@@ -0,0 +1,10 @@
+#Scrivete una funzione che sommi tutti gli elementi di una lista*.
+#Poi, committate il file in cui l’avete scritta.
+
+#inizializzo la lista
+list = [1,2,3,4,5,6,7,8,9,]
+
+somma = sum(list)
+print ("somma:{}" .format(somma))
+
+
